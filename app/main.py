5cff1bcbae511403acd7c7680c597e2558409c72
Krish <52761222+Krish120003@users.noreply.github.com>
2022-12-30 16:29:24 -0500

    add basic set and get

--- app/main.py
+++ app/main.py
@@ -1,18 +1,46 @@
 import socket
-from threading import Thread
+from threading import Thread, Lock
 
 RECV_BLOCK_SIZE = 1024
 
+data = {}
+data_lock = Lock()
+
 
 def responder(socket):
+    global data
     while payload := socket.recv(RECV_BLOCK_SIZE):
 
         payload = payload.decode("utf-8").strip().split("\r\n")[1::][1::2]
         command = payload[0]
-        print(command)
         if command == "echo":
             socket.send(f"+{' '.join(payload[1:])}\r\n".encode("utf-8"))
+        elif command == "set":
+            # we should lock the data here
+            data_lock.acquire()
+            key = payload[1]
+            value = payload[2]
+            data[key] = value
+            # now we are done with the data, we can release the lock
+            data_lock.release()
+            socket.send(f"+OK\r\n".encode("utf-8"))
+        elif command == "get":
+            key = payload[1]
+            # we should lock the data here
+            data_lock.acquire()
+            if key in data:
+                value = data[key]
+                # now we are done with the data, we can release the lock
+                data_lock.release()
+                socket.send(
+                    (f"${len(value)}\r\n" +
+                     value + "\r\n").encode("utf-8")
+                )
+            else:
+                data_lock.release()
+                socket.send(b"$-1\r\n")
         else:
+            print(payload)
             socket.send(b"+PONG\r\n")
 
 
